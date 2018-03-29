7e690aed72a025fecce959488d6c2d5d40ead93f
Rossprehn <32393462+Rossprehn@users.noreply.github.com>
2018-03-29 10:39:17 -0600

    fixed seeds

--- seeds/01_meditations.js
+++ seeds/01_meditations.js
@@ -8,109 +8,115 @@ exports.seed = function(knex, Promise) {
           uri: 'https://www.youtube.com/watch?v=taxs_vhlxgs',
           type: 'Morning',
           length: 'Short',
-          voice: 'Vocal'
+          voice: 'Spoken'
         },
         {
           id: 2,
           uri: 'https://www.youtube.com/watch?v=WYP_W49o1vQ',
           type: 'Morning',
           length: 'Medium',
-          voice: 'Vocal'
+          voice: 'Spoken'
         },
         {
           id: 3,
           uri: 'https://www.youtube.com/watch?v=OboD7JrT0NE',
           type: 'Morning',
           length: 'Long',
-          voice: 'Vocal'
+          voice: 'Spoken'
         },
         {
           id: 4,
-          uri: 'https://www.youtube.com/watch?v=OboD7JrT0NE',
+          uri: 'https://www.youtube.com/watch?v=KZHSR5VG0uU',
           type: 'Morning',
           length: 'Short',
           voice: 'Music'
         },
         {
           id: 5,
-          uri: 'https://www.youtube.com/watch?v=OboD7JrT0NE',
+          uri: 'https://www.youtube.com/watch?v=K-McnOai_Zo',
           type: 'Morning',
           length: 'Medium',
           voice: 'Music'
         },
         {
           id: 6,
-          uri: 'https://www.youtube.com/watch?v=OboD7JrT0NE',
+          uri: 'https://www.youtube.com/watch?v=nrXuS5czlF8',
           type: 'Morning',
           length: 'Long',
           voice: 'Music'
         },
         {
           id: 7,
-          uri: 'https://www.youtube.com/watch?v=OboD7JrT0NE',
+          uri:
+            'https://www.youtube.com/watch?v=cFfP7N8wpMA&t=0s&index=2&list=PLZf3NUXeOhUuWVnux1k6u6aGBh9_iqo0u',
           type: 'Motivation',
           length: 'Short',
-          voice: 'Vocal'
+          voice: 'Spoken'
         },
         {
           id: 8,
-          uri: 'https://www.youtube.com/watch?v=OboD7JrT0NE',
+          uri: 'https://www.youtube.com/watch?v=_Ub9kncj5J0&t=72s',
           type: 'Motivation',
           length: 'Medium',
-          voice: 'Vocal'
+          voice: 'Spoken'
         },
         {
           id: 9,
-          uri: 'https://www.youtube.com/watch?v=OboD7JrT0NE',
+          uri:
+            'https://www.youtube.com/watch?v=sP7CChezoN4&t=0s&index=5&list=PLZf3NUXeOhUuWVnux1k6u6aGBh9_iqo0u',
           type: 'Motivation',
           length: 'Long',
-          voice: 'Vocal'
+          voice: 'Spoken'
         },
         {
           id: 10,
-          uri: 'https://www.youtube.com/watch?v=OboD7JrT0NE',
+          uri: 'https://www.youtube.com/watch?v=8IjzV3mt2T8',
           type: 'Motivation',
           length: 'Short',
-          voice: 'Vocal'
+          voice: 'Music'
         },
         {
           id: 11,
-          uri: 'https://www.youtube.com/watch?v=OboD7JrT0NE',
+          uri: 'https://www.youtube.com/watch?v=yQLCWiekZV0',
           type: 'Sleep',
           length: 'Short',
-          voice: 'Vocal'
+          voice: 'Spoken'
         },
         {
           id: 12,
-          uri: 'https://www.youtube.com/watch?v=OboD7JrT0NE',
+          uri:
+            'https://www.youtube.com/watch?v=y8LIbeKQ60U&t=0s&index=8&list=PLZf3NUXeOhUuWVnux1k6u6aGBh9_iqo0u',
           type: 'Sleep',
           length: 'Medium',
-          voice: 'Vocal'
+          voice: 'Spoken'
         },
         {
           id: 13,
-          uri: 'https://www.youtube.com/watch?v=OboD7JrT0NE',
+          uri:
+            'https://www.youtube.com/watch?v=q76bMs-NwRk&t=0s&index=11&list=PLZf3NUXeOhUuWVnux1k6u6aGBh9_iqo0u',
           type: 'Sleep',
           length: 'Long',
-          voice: 'Vocal'
+          voice: 'Spoken'
         },
         {
           id: 14,
-          uri: 'https://www.youtube.com/watch?v=OboD7JrT0NE',
+          uri: 'https://www.youtube.com/watch?v=asg8XMzOvCE',
           type: 'Sleep',
           length: 'Short',
           voice: 'Music'
         },
         {
           id: 15,
-          uri: 'https://www.youtube.com/watch?v=OboD7JrT0NE',
+          uri:
+            'https://www.youtube.com/watch?v=N4qCFFBrrgk&t=0s&index=12&list=PLZf3NUXeOhUuWVnux1k6u6aGBh9_iqo0u',
           type: 'Sleep',
           length: 'Medium',
           voice: 'Music'
         },
         {
           id: 16,
-          uri: 'https://www.youtube.com/watch?v=OboD7JrT0NE',
+          uri:
+            'https://www.youtube.com/watch?v=CVvdAWp37jw&t=0s&index=10&list=PLZf3NUXeOhUuWVnux1k6u6aGBh9_iqo0u',
           type: 'Sleep',
           length: 'Long',
           voice: 'Music'
